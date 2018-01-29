4b6e2e764f8926e9880c386da62f1aeab4c1a73a
marwis95 <marwis95@o2.pl>
2018-01-29 11:59:28 -0800

    Ukryj/Pokaz

---
+++ pokaz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+
+mv .db.txt db.txt
+
+echo "Baza danych zostala odkryta"
+
+echo "Nacisnij dowolny klawisz aby wrocic do menu"
+read key
+bash main.sh
---
+++ ukryj.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+
+mv db.txt .db.txt
+
+echo "Baza danych zostala ukryta"
+
+echo "Nacisnij dowolny klawisz aby wrocic do menu"
+read key
+bash main.sh
